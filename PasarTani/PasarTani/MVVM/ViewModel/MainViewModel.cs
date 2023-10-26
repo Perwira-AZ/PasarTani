a7ad0499cb171dae9c641100f6ebf96be385c4b6
adhityabye <mr.adhityabayu@gmail.com>
2023-10-26 19:37:51 +0700

    add initialization logic for main page

---
+++ PasarTani/PasarTani/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using PasarTani.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasarTani.MVVM.ViewModel
+{
+    internal class MainViewModel: Core.ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand SellerViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public SellerViewModel SellerVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            SellerVM = new SellerViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            SellerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SellerVM;
+            });
+        }
+    }
+}
