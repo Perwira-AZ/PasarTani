73571968008c2ba267c8bdcb777c713d21052e77
adhityabye <mr.adhityabayu@gmail.com>
2023-11-10 07:26:36 +0700

    reinitialize class model

---
+++ PasarTani/PasarTani/MVVM/Model/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasarTani.Model
+{
+    internal class Item
+    {
+        private int _itemID;
+        private string _itemName;
+        private int _stock;
+        private uint _price;
+
+        public int ItemID
+        {
+            get { return _itemID; }
+        }
+        public string ItemName
+        {
+            get { return _itemName; }
+            set { _itemName = value; }
+        }
+        public int Stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+        public uint Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
