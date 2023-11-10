9de08d519812d99dca34a352b613fd8be38c10f2
adhityabye <mr.adhityabayu@gmail.com>
2023-11-10 08:12:13 +0700

    seller customer interface

---
+++ PasarTani/PasarTani/MVVM/Model/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasarTani.MVVM.Model
+{
+    internal interface IUser
+    {
+        int UserID { get; }
+        string Name { get; set; }
+        string PhoneNumber { get; }
+        string Email { get; }
+        string Address { get; set; }
+        bool Authenticate(string password, string email);
+    }
+}
