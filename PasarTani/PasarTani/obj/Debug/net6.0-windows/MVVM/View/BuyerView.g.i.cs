9e0358a7840099826ad42ea34d14b0002b699173
adhityabye <mr.adhityabayu@gmail.com>
2023-12-01 06:27:05 +0700

    detailed item

--- PasarTani/PasarTani/obj/Debug/net6.0-windows/MVVM/View/BuyerView.g.i.cs
+++ PasarTani/PasarTani/obj/Debug/net6.0-windows/MVVM/View/BuyerView.g.i.cs
@@ -48,13 +48,13 @@ namespace PasarTani.MVVM.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PasarTani;component/mvvm/view/buyerview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PasarTani;V1.0.0.0;component/mvvm/view/buyerview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\BuyerView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -64,7 +64,7 @@ namespace PasarTani.MVVM.View {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
