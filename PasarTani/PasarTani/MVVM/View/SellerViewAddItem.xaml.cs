6fa9eccb320bd3ea08d4991dbfbfaa778d39780c
Nur-Azzam-Wafiuddin <nur.azzam.wafiuddin@mail.ugm.ac.id>
2023-12-01 03:14:08 +0700

    Frontend Add Item to Seller Dashboard

---
+++ PasarTani/PasarTani/MVVM/View/SellerViewAddItem.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using PasarTani.Model;
+using PasarTani.MVVM.Model;
+using PasarTani.MVVM.Services;
+using PasarTani.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Imagekit;
+
+namespace PasarTani.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for SellerViewAddItem.xaml
+    /// </summary>
+    public partial class SellerViewAddItem : Window
+    {
+        public SellerViewAddItem()
+        {
+            InitializeComponent();
+            this.DataContext = new SellerViewAddItemModel(); ;
+        }
+
+        private void btnOpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+
+            string imageurl = "";
+
+
+
+            if (op.ShowDialog() == true)
+            {
+                uploadImgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+
+                Trace.WriteLine(op.FileName);
+
+                ItemServices itemServices = new ItemServices();
+
+                imageurl = itemServices.GenerateUrlImage(op.FileName, SharedData.currentAccountLoginID + SharedData.currentAccountName);
+
+                Trace.WriteLine(imageurl); // Store this url to postgreSQL
+
+                SharedData.temporaryUploadImage = imageurl;
+
+            }
+
+        }
+ 
+    }
+}
---
+++ PasarTani/PasarTani/MVVM/ViewModel/SellerViewAddItemModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using PasarTani.Core;
+using PasarTani.Model;
+using PasarTani.MVVM.Model;
+using PasarTani.MVVM.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Xml.Linq;
+
+namespace PasarTani.MVVM.ViewModel
+{
+    internal class SellerViewAddItemModel
+    {
+        ItemServices itemServices = new ItemServices();
+        public ICommand AddItemCommand { get; set; }
+        public string? Name { get; set; }
+        public string? Stock { get; set; }
+        public string? Price { get; set; }
+
+
+        public SellerViewAddItemModel()
+        {
+            AddItemCommand = new RelayCommand(AddItem, CanAddItem);
+        }
+
+        private bool CanAddItem(object obj)
+        {
+            return true;
+        }
+
+        private void AddItem(object obj)
+        {
+            string imageUrl = SharedData.temporaryUploadImage;
+
+            if(SharedData.currentAccountLoginID != 0 && SharedData.isAccountSeller == true)
+            {
+                Trace.WriteLine("Approval Mode");
+                int sellerID = SharedData.currentAccountLoginID;
+                itemServices.AddItem(Name, sellerID, int.Parse(Stock), decimal.Parse(Price), imageUrl);
+            }
+
+        }
+    }
+}
