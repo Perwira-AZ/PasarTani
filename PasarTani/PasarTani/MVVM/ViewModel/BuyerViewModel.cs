fd48da36df1d5ccf1d81f27e50d87c69b6008c2c
Nur-Azzam-Wafiuddin <nur.azzam.wafiuddin@mail.ugm.ac.id>
2023-12-01 11:44:50 +0700

    (NEED FIX, detail still include edit and delete)Add detail item in buyer view

--- PasarTani/PasarTani/MVVM/ViewModel/BuyerViewModel.cs
+++ PasarTani/PasarTani/MVVM/ViewModel/BuyerViewModel.cs
@@ -1,11 +1,15 @@
-﻿using PasarTani.Model;
+﻿using PasarTani.Core;
+using PasarTani.Model;
 using PasarTani.MVVM.Services;
+using PasarTani.MVVM.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace PasarTani.MVVM.ViewModel
 {
@@ -13,11 +17,15 @@ namespace PasarTani.MVVM.ViewModel
     {
         private readonly ItemServices _itemServices = new ItemServices();
 
+        public ICommand ShowDetailItem { get; set; }
+
         public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
 
         public BuyerViewModel()
         {
             LoadItems();
+
+            ShowDetailItem = new RelayCommand(ShowDetail, CanShowDetail);
         }
 
         private void LoadItems()
@@ -27,6 +35,21 @@ namespace PasarTani.MVVM.ViewModel
             {
                 Items.Add(item);
             }
+        }
+
+        private bool CanShowDetail(object obj)
+        {
+            return true;
+        }
+
+        private void ShowDetail(object obj)
+        {
+            var detailWindow = obj as Window;
+
+            DetailItem detail = new DetailItem();
+            detail.Owner = detailWindow;
+            detail.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            detail.Show();
         }
     }
 }
